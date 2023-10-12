9918208cde50dfc6d4e605d40bdf6bba288be8ff
Sangyeop Kim <sangykimiv@gmail.com>
2023-10-12 21:59:09 +0900

    feat: update RegisterUserPort
    added saveUser method for saving user entity

--- src/main/java/com/psymoney/authserver/user/application/port/out/RegisterUserPort.java
+++ src/main/java/com/psymoney/authserver/user/application/port/out/RegisterUserPort.java
@@ -1,5 +1,8 @@
 package com.psymoney.authserver.user.application.port.out;
 
+import com.psymoney.authserver.user.domain.User;
+
 public interface RegisterUserPort {
     boolean isUsernameExisted(String username);
+    boolean saveUser(User user);
 }
