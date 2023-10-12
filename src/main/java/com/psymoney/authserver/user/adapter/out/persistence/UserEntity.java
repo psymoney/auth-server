e7697ea3b2f47b76bbb56ec8fb103fec10e97d9e
Sangyeop Kim <sangykimiv@gmail.com>
2023-10-12 17:56:45 +0900

    chore: update UserEntity
    added lombok data annotation

--- src/main/java/com/psymoney/authserver/user/adapter/out/persistence/UserEntity.java
+++ src/main/java/com/psymoney/authserver/user/adapter/out/persistence/UserEntity.java
@@ -1,10 +1,11 @@
 package com.psymoney.authserver.user.adapter.out.persistence;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.List;
 
-
+@Data
 @Entity
 @Table(name = "users")
 public class UserEntity {
