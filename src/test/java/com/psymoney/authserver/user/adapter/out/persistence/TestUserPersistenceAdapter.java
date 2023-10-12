68629684b535252c61ecf23fe50a0958de7c6ef9
Sangyeop Kim <sangykimiv@gmail.com>
2023-10-12 22:00:28 +0900

    test: update TestUserPersistenceAdapter
    added test case for saveUser method
    included required dependencies

--- src/test/java/com/psymoney/authserver/user/adapter/out/persistence/TestUserPersistenceAdapter.java
+++ src/test/java/com/psymoney/authserver/user/adapter/out/persistence/TestUserPersistenceAdapter.java
@@ -1,5 +1,6 @@
 package com.psymoney.authserver.user.adapter.out.persistence;
 
+import com.psymoney.authserver.user.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,11 +10,12 @@ import org.springframework.test.context.jdbc.Sql;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@Import(UserPersistenceAdapter.class)
+@Import({UserPersistenceAdapter.class, UserMapper.class})
 @Sql(scripts = "classpath:sql/test-user-data-h2.sql")
 public class TestUserPersistenceAdapter {
     @Autowired private UserRepository userRepository;
     @Autowired private UserPersistenceAdapter userPersistenceAdapter;
+    @Autowired private UserMapper userMapper;
 
     @Test
     void testLoadedUsers() {
@@ -35,5 +37,22 @@ public class TestUserPersistenceAdapter {
         boolean isExisted = userPersistenceAdapter.isUsernameExisted("user");
 
         assertThat(isExisted).isTrue();
+    }
+
+    @Test
+    void testSaveUser() {
+        User givenUser = new User("test", "testPassword");
+        UserEntity expectedUserEntity = new UserEntity();
+        expectedUserEntity.setUsername("test");
+        expectedUserEntity.setPassword("testPassword");
+        expectedUserEntity.setEnabled(true);
+
+        boolean isSaved = userPersistenceAdapter.saveUser(givenUser);
+        UserEntity actualUserEntity = userRepository.findByUsername("test");
+
+        assertThat(isSaved).isTrue();
+        assertThat(actualUserEntity.isEnabled()).isEqualTo(expectedUserEntity.isEnabled());
+        assertThat(actualUserEntity.getPassword()).isEqualTo(expectedUserEntity.getPassword());
+        assertThat(actualUserEntity.getUsername()).isEqualTo(expectedUserEntity.getUsername());
     }
 }
